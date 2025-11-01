5bde9d2fb8d579984bb0798e402a47fc47363031
jinug0905 <60326817+jinug0905@users.noreply.github.com>
2025-11-01 17:56:17 -0500

    Update main.cpp
    Updated with some more comments

--- 2_EstimateMem/main.cpp
+++ 2_EstimateMem/main.cpp
@@ -8,15 +8,27 @@
 #include <torch/csrc/jit/passes/shape_analysis.h>
 #include <torch/csrc/jit/passes/freeze_module.h>
 
-
 #include <ATen/ATen.h>
 
 #include <iostream>
 #include <unordered_map>
 #include <vector>
 #include <sstream>
+#include <cmath>
+#include <string>
 
-// ----- Helpers -----
+
+/*
+  C10 : pytorch core c++ library. It provides base components like Tensor data types.
+    - c10::TensorImpl, c10::Device, c10::ScalarType
+    - c10::Dispatcher, device handling (CPU, CUDA, XLA, etc.)
+
+    Python API  ──>  ATen (tensor ops)
+                 ↑
+               c10 (core runtime, dispatch, types)
+                 ↑
+              JIT / Autograd sit alongside
+*/
 static size_t scalar_type_size(c10::ScalarType t) {
   return c10::elementSize(t); // bytes per element
 }
@@ -35,22 +47,26 @@ static size_t tensor_type_nbytes(const c10::VaryingShape<int64_t>& sizes, c10::S
 }
 
 
-// How to display in recognizable form
-static std::string pretty(size_t n) {
-  const char* units[] = {"B","KB","MB","GB","TB"};
+// // How to display in recognizable form
+// static std::string pretty(size_t n) {
+//   const char* units[] = {"B","KB","MB","GB","TB"};
 
-  double d = static_cast<double>(n);
-  int u = 0;
-  while (d >= 1024.0 && u < 4) {
-    d /= 1024.0;
-    ++u;
-  }
+//   double d = static_cast<double>(n);
+//   int u = 0;
+//   while (d >= 1024.0 && u < 4) {
+//     d /= 1024.0;
+//     ++u;
+//   }
 
-  std::ostringstream oss; oss.setf(std::ios::fixed); oss.precision(5);
-  oss << d << " " << units[u];
-  return oss.str();
+//   std::ostringstream oss; oss.setf(std::ios::fixed); oss.precision(5);
+//   oss << d << " " << units[u];
+//   return oss.str();
+// }
+
+static std::string bytes_to_mb(size_t n){
+  double mb = static_cast<double>(n) / (1024.0 * 1024.0);
+  return std::to_string(mb);
 }
-
 
 /*
   Compute liveness of tensor : where does a tensor die???
@@ -83,113 +99,69 @@ static std::unordered_map<const torch::jit::Value*, size_t> compute_last_use_ind
   return lastUse;
 }
 
-/*
-  PyTorch’s memory management's 2 main categories of allocations:
-  ----------------------------------------------------------------------------------
-  Persistent: Model parameters and buffers
-
-    - Allocated once when the model is built (or loaded)  
-    - Always live (until you delete or move the module),
-
-  Transient : Activations, temporary tensors, intermediate outputs
-
-    - Allocated during the forward/backward pass, released when no longer needed
-      - Freed logically when the tensor is destroyed,
-        but the physical GPU memory is cached for reuse.
-    - Follows tensor liveness determined by Python scope and autograd graph
-*/
-
 int main() {
   try {
 
-    /*
-      load TorchScript module from .pt file
-      it contains
-        - method bytecode/IR (e.g. forward)
-        - the module hierarchy and attributes
-        - parameters/buffers
-          - parameters : this is learnable components in NN
-            - requires_grad=True is default
-            - ex : usually means weights and biases.
-          - buffers : these are tensor values that are associated with a module's state but not learnable
-            - requires_grad=False is default
-            - ex : running mean and variance in a batch normalization layer in nn.BatchNorm2d
+/*
+    Torch Script module : serialized, language-independent representation of a PyTorch model (compiled form of a torch.nn.Module)
 
-      module.get_method("forward").graph() -> give high-level graph for forward pass
+    zip contains : 
+    model.pt.zip
+    ├── data.pkl                # parameters & buffers
+    ├── code/                   # TorchScript code
+    │   ├── __torch__.py        # scripted code representation
+    │   └── ...
+    ├── constants.pkl
+    ├── version                 # IR version
+    └── bytecode.pkl            # JIT-optimized VM instructions
 
-      method.graph() -> Graph object describe IR
-        - nodes (ops like aten::linear, aten::relu, prim::GetAttr)
-        - inputs/outputs (graph->inputs(), graph->outputs())
-        - SSA values (Value*) and their Types
-    */
+        - Code : Forward function, submodule methods (in IR form)
+        - Parameters : Learnable tensors (weights, biases)
+        - Buffers : Non-learnable tensors (running_mean, running_var)
+        - JIT Graph / IR : Static computation graph for execution + optimization
+        - Type info : Module structure information
+        - Serialized constants : Shapes, integer constants, etc.
 
-    const std::string ts_path = std::string(DATA_DIR) + "/vgg16b1.pt";
+    2 forms exist : 
+        - tracing : Observe execution with sample input (torch.jit.trace)
+            => used in simple feed-forward ops
+
+        - Scripting : Compile Python to TorchScript (torch.jit.script)
+            => Dynamic control flow, loops, if-statements
+
+    3. Example form (Simple CNN)
+
+    graph(%self.1 : __torch__.MyConv,
+      %input.1 : Tensor):
+        %2 : Tensor = prim::GetAttr[name="weight"](%self.1)
+        %3 : Tensor = prim::GetAttr[name="bias"](%self.1)
+        %4 : Tensor = aten::conv2d(%input.1, %2, %3, [1,1], [0,0])
+        %5 : Tensor = aten::relu(%4)
+        return (%5)
+*/
+
+    const std::string ts_path = std::string(DATA_DIR) + "/resnet50b1_script.pt";
     torch::jit::Module module = torch::jit::load(ts_path);
 
-    size_t paramBytes = 0;
-    for (const auto& p : module.named_parameters(/*recurse=*/true)) {
-      const auto& t = p.value;
-      paramBytes += (size_t)t.numel() * t.element_size();
-    }
-
-    for (const auto& b : module.named_buffers(/*recurse=*/true)) {
-      const auto& t = b.value;
-      paramBytes += (size_t)t.numel() * t.element_size();
-    }
-
     /*
-      freeze to inline submodules & fold attributes
-      If no freeze, it will create IR like %30 : Tensor = prim::CallMethod[name="forward"](%relu.1, %26) # :0:0
-      which hides all the nodes (relu) behind
-      This will save both frozend version and original JIT module, so memory occupation increase.
-    */
-    module = torch::jit::freeze_module(std::move(module));
+        PyTorch’s memory management's 2 main categories of allocations:
 
-    auto method = module.get_method("forward");
-    std::shared_ptr<torch::jit::Graph> graph = method.graph();
+        1. Persistent: Model parameters and buffers
 
-    // Seed concrete input type (use CPU or CUDA as you prefer)
-    // auto* xin = graph->inputs().at(1);
-    // at::Tensor dummy = at::empty({1,3,224,224}, at::device(c10::kCPU).dtype(at::kFloat));
-    // xin->setType(c10::TensorType::create(dummy));
+        - Allocated once when the model is built (or loaded)  
+        - Always live (until you delete or move the module),
+        - requires_grad=True is default
+        - ex : usually means weights and biases.
 
-    // std::cout << "Checking graph : " << graph->toString() << "\n";
-    // std::cout << std::endl;
+        2. Transient : Activations, temporary tensors, intermediate outputs
 
-    /*
-    1. Inline to flatten the graph so it looks like IR (Just like one straigh list of ops)
+        - Allocated during the forward/backward pass, released when no longer needed
+        - Freed logically when the tensor is destroyed, but the physical GPU memory is cached for reuse.
+        - Follows tensor liveness determined by Python scope and autograd graph
+        - requires_grad=False is default
+        - ex : running mean and variance in a batch normalization layer in nn.BatchNorm2d
 
-    2. Constant propagation : folds constatns through graph
-    	- If a node’s outputs depend only on known constants, it precomputes and replaces with literal constants.=
-	    - It prune dead constant subgraphs
-
-    3. PropagateInputShapes : populate graph types values for each tensor value(like %10, %11)
-      - Applies per-op rules (e.g., linear, relu, conv) to infer TensorType (dtype, device, sizes)
-      - Writes the inferred TensorType onto each Value
-
-      Which makes each TensorType has
-        - Scalar Type(float, double)
-        - sizes (8 or 16 or ...)
-        - device info and flags like require grads
-
-      Before :
-        %10 : Tensor = aten::linear(%x.1, %weight.2, %bias.2)
-        %11 : Tensor = aten::relu(%10)
-        %14 : Tensor = aten::linear(%11, %weight.1, %bias.1)
-
-      After : 
-        %x.1  : Float(8, 32, ...)
-        %10   : Float(8, 64, ...)
-        %11   : Float(8, 64, ...)
-        %14   : Float(8, 10, ...)
-    */
-
-    torch::jit::Inline(*graph);
-    torch::jit::ConstantPropagation(graph);
-    torch::jit::PropagateInputShapes(graph);
-
-    /*
-      Sum the parameters
+    Sum the parameters
 
       module.named_parameters (learnable, gradient optimizable)
         : weights and bias (doesn't free memory as pass)
@@ -200,7 +172,7 @@ int main() {
         : batch norm calculating mean and variance, positional encoding, masking
         : Same logic as parameters, but it will return nothing for current simple implementation
 
-      In detail
+    In detail
 
       For parameters, if written like 
         self.fc1 = nn.Linear(32, 64)
@@ -220,19 +192,140 @@ int main() {
       model._buffers     # dict of name → tensor
     */
 
-    // size_t paramBytes = 0;
-    // for (const auto& p : module.named_parameters(/*recurse=*/true)) {
-    //   const auto& t = p.value;
-    //   paramBytes += static_cast<size_t>(t.numel()) * t.element_size();
-    // }
-    // for (const auto& b : module.named_buffers(/*recurse=*/true)) {
-    //   const auto& t = b.value;
-    //   paramBytes += static_cast<size_t>(t.numel()) * t.element_size();
-    // }
+    size_t paramBytes = 0;
+    for (const auto& p : module.named_parameters(/*recurse=*/true)) {
+      const auto& t = p.value;
+      paramBytes += (size_t)t.numel() * t.element_size();
+    }
+
+    for (const auto& b : module.named_buffers(/*recurse=*/true)) {
+      const auto& t = b.value;
+      paramBytes += (size_t)t.numel() * t.element_size();
+    }
+
+    /*
+        Freeze the Torch Script module
+            - Removes prim::CallMethod calls
+                If no freeze, it will create IR like %30 : Tensor = prim::CallMethod[name="forward"](%relu.1, %26) # :0:0
+            - Turns parameters → constants, if possiible
+            - Fold submodules if static
+
+            Before :
+                graph(%self, %x):
+                    %h = prim::CallMethod[name="layer1"](%self, %x)
+                    return (%h)
+            
+            After : 
+                graph(%x):
+                    %w = prim::GetAttr[name="conv1.weight"](%self)
+                    %1 = aten::conv2d(%x, %w, ...)
+                    %2 = aten::relu(%1)
+                    ...
+                    return (%2)
+    */
+    module = torch::jit::freeze_module(std::move(module));
+
+    /*
+        module.get_method("forward").graph() -> give high-level graph for forward pass (inference)
+        Backward part is saved in PyTorch’s Autograd engine.
+
+        method.graph() -> Graph object describe IR
+            - nodes (ops like aten::linear, aten::relu, prim::GetAttr)
+            - inputs/outputs (graph->inputs(), graph->outputs())
+            - SSA values (Value*) and their Types
+            
+            we can use graph->nodes() to traverse nodes to do calculation.
+    */
+
+    auto method = module.get_method("forward");
+    std::shared_ptr<torch::jit::Graph> graph = method.graph();
+
+    // std::cout << "Checking graph : " << graph->toString() << "\n";
+    // std::cout << std::endl;
+
+    /*
+        1. Inline to flatten the graph so it looks like IR (Just like one straigh list of ops)
+        
+        if, 
+            def foo(a):
+                return a + 7
+
+        Before : 
+            graph(%x):
+                %y = prim::CallFunction(fn=foo)(%x)
+                return (%y)
+        
+        After : 
+            graph(%x):
+                %2 = aten::add(%x, 7)
+                return (%2)
+
+        2. Constant propagation : folds constatns through graph
+    	- If a node’s outputs depend only on known constants, it precomputes and replaces with literal constants.=
+	    - It prune dead constant subgraphs
+
+        Before : 
+            graph():
+                %a = prim::Constant[value=3]()
+                %b = prim::Constant[value=4]()
+                %c = aten::mul(%a, %b)
+                return (%c)
+        
+        After : 
+            graph():
+                %0 = prim::Constant[value=12]()
+                return (%0)
+
+        3. PropagateInputShapes : populate graph types values for each tensor value(like %10, %11)
+        - Applies per-op rules (e.g., linear, relu, conv) to infer TensorType (dtype, device, sizes)
+        - Infer tensor shapes and types through IR based on known input shapes.
+
+        Which makes each TensorType has
+        - Scalar Type(float, double)
+        - sizes (8 or 16 or ...)
+        - device info and flags like require grads
+
+        Ex 01)
+        Before :
+        %10 : Tensor = aten::linear(%x.1, %weight.2, %bias.2)
+        %11 : Tensor = aten::relu(%10)
+        %14 : Tensor = aten::linear(%11, %weight.1, %bias.1)
+
+        After : 
+        %x.1  : Float(8, 32, ...)
+        %10   : Float(8, 64, ...)
+        %11   : Float(8, 64, ...)
+        %14   : Float(8, 10, ...)
+
+        Ex 02)
+
+        Before : 
+        graph(%x):
+            %y = aten::relu(%x)
+            return (%y)
+
+        After :
+        graph(%x : Float(1,3,224,224)):
+            %y : Float(1,3,224,224) = aten::relu(%x)
+            return (%y)
+    */
+
+    /*
+        Read in size of parameters (parameters/buffers) before freeze because 
+        torch::jit::freeze folds any immutable parameters/buffers into IR constants and drops those attributes from the module.
+        Which will end up
+	    - module.named_parameters(recurse=true) will no longer include any params that were folded.
+	    - module.named_buffers(recurse=true) will shrink if buffers (e.g., BatchNorm running stats) were folded.
+    */
+
+    torch::jit::Inline(*graph);                 // Done in freeze part so redundant
+    torch::jit::ConstantPropagation(graph);     // Done in freeze part so redundant
+    torch::jit::PropagateInputShapes(graph);
 
     /*
       Topological ordering
-      Since simple MLP is simple, the order is as it is shown in ir.txt
+      TorchScript maintains nodes in a valid topological traversal order in its execution block
+      -> within a straight-line graph (no control flow branches), node list is in topological order and can be consumed in sequence to simulate foward order execution
 
       Node (torch::jit::Node) is object inside Graph(torch::jit::Graph) which represent each ops
       (like aten::linear, aten::relu, prim::GetAttr, …)
@@ -257,8 +350,38 @@ int main() {
 
     /*
       we save them in vector later we need random access (die or live)
-      (e.g. to find a node’s numeric position, or look up by index),
+      Fast lookup for node lifetimes (where outputs get consumed)
       which the graph->nodes() iterator doesn’t support.
+
+      Ex)
+
+      if model graph :
+      -=-=-=-=-=-=-=-=-=-=-
+      %0 = conv2d
+      %1 = relu
+      %2 = max_pool2d
+      %3 = layer1 (ops ...)
+      %4 = layer2 (ops ...)
+      %5 = adaptive_avg_pool2d
+      %6 = linear
+
+      topo will have :
+      -=-=-=-=-=-=-=-=-=-=-
+      topo[0] = Node(conv2d)
+      topo[1] = Node(relu)
+      topo[2] = Node(max_pool2d)
+      topo[3] = Node(conv2d layer1)
+      topo[4] = Node(relu layer1)
+      topo[5] = Node(linear)
+
+      nodeIdx will have :
+      -=-=-=-=-=-=-=-=-=-=-
+      Node(conv2d)         , 0
+      Node(relu)           , 1
+      Node(max_pool2d)     , 2
+      Node(conv2d layer1)  , 3
+      Node(relu layer1)    , 4
+      Node(linear)         , 5
     */
 
     std::vector<torch::jit::Node*> topo;
@@ -270,11 +393,15 @@ int main() {
     }
 
     /*
-      Checking liveness : to track when is the last use for each tensor output (how long alive)
-      topo : node in topological order
-      nodeIdx : fast look up for A node*. at what step does node N run?
+        Checking liveness : to track when is the last use for each tensor output (how long alive)
+        topo : node in topological order
+        nodeIdx : fast look up for A node*. at what step does node N run?
+        last use : where tensor die??
 
-      last use : where tensor die??
+        Ex) 
+        Tensor      Produced at     Last use
+        %0          idx 0           idx 1
+        %1          idx 1           idx 3
     */
     auto lastUse = compute_last_use_indices(topo, nodeIdx);
 
@@ -291,41 +418,54 @@ int main() {
     for (size_t i = 0; i < topo.size(); ++i) {
       auto* n = topo[i];
 
-      // New outputs become live
-      for (auto* v : n->outputs()) {
-        auto tensorType = v->type()->cast<c10::TensorType>();
-        if (!tensorType) continue; // skip non tensor
+    /*
+      n->outputs() exerts SSA vals for each node.
+      From 
+          %1 : Tensor = aten::conv2d(%x, %w, ...)
+          %2 : Tensor = aten::relu(%1)
+      
+      we have 2 nodes : conv2d and relu
+      conv2d outputs %1
+      relu outputs %2
 
-        auto tensorSt = tensorType->scalarType();
-        if (!tensorSt.has_value()) continue; // skip unknown dtype
+      %1 and %2 is torch::jit::Value*.
+      which saves Shape (TensorType::sizes()), Data type (TensorType::scalarType()), Device (TensorType::device())
+    */
 
-        size_t bytes = tensor_type_nbytes(tensorType->sizes(), *tensorSt);
-        if (bytes == 0) continue; // skiip unknown shape
+    for (auto* v : n->outputs()) { 
+      auto tensorType = v->type()->cast<c10::TensorType>();
+      if (!tensorType) continue; // Skip non-tensor outputs (shapes, bool flags) -> doesn't need memory allocation
 
-        size_t dieAt = lastUse.count(v) ? lastUse[v] : i; // default is die now (ex last node)
-        liveSet.push_back({v, bytes, dieAt});
-        liveBytes += bytes;
+      auto tensorSt = tensorType->scalarType();
+      if (!tensorSt.has_value()) continue;    // skip unknown dtype
 
-        if (liveBytes > peakActBytes)
-          peakActBytes = liveBytes;
-      }
+      size_t bytes = tensor_type_nbytes(tensorType->sizes(), *tensorSt);
+      if (bytes == 0) continue; // skiip unknown shape (TensorType(shape=[?, ?, ?, ?], dtype=float)) could be error
 
-      // Kill values whose last use is this node
-      for (auto it = liveSet.begin(); it != liveSet.end();) {
-        if (it->dieAt == i) {
-          liveBytes -= it->bytes;
-          it = liveSet.erase(it);
+      size_t dieAt = lastUse.count(v) ? lastUse[v] : i; // default is die now (ex last node)
+      liveSet.push_back({v, bytes, dieAt});
+      liveBytes += bytes;
 
-        } else {
-          ++it;
-        }
-      }
+      if (liveBytes > peakActBytes)
+        peakActBytes = liveBytes;
     }
 
+    // Kill values whose last use is this node
+    for (auto it = liveSet.begin(); it != liveSet.end();) {
+      if (it->dieAt == i) {
+        liveBytes -= it->bytes;
+        it = liveSet.erase(it);
+
+      } else {
+        ++it;
+      }
+    }
+  }
+
     const size_t totalPeak = paramBytes + peakActBytes;
-    std::cout << "Parameter bytes:        " << pretty(paramBytes) << "\n";
-    std::cout << "Peak activations:       " << pretty(peakActBytes) << "\n";
-    std::cout << "Total peak (inference): " << pretty(totalPeak) << "\n";
+    std::cout << "Parameter bytes(MB):        " << bytes_to_mb(paramBytes) << "\n";
+    std::cout << "Peak activations(MB):       " << bytes_to_mb(peakActBytes) << "\n";
+    std::cout << "Total peak (inference): " << bytes_to_mb(totalPeak) << "\n";
     return 0;
 
   } catch (const c10::Error& e) {
